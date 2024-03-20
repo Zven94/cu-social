a6157e1a1e867832497b283606e9e78a467eeea5
Zven94 <nicobolanez@gmail.com>
2024-03-20 18:03:10 -0300

    fix claim_description column name

--- db/migrate/20240319205451_create_claims.rb
+++ db/migrate/20240319205451_create_claims.rb
@@ -2,7 +2,7 @@ class CreateClaims < ActiveRecord::Migration[7.1]
   def change
     create_table :claims do |t|
       t.string :claim_name
-      t.string :claims_img
+      t.string :claim_img
       t.string :claim_description
       t.integer :claim_votes, default: 0
       t.boolean :claim_status, default: false
